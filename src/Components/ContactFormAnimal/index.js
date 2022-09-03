e638cffc17128bc583fe6ac261bf66da79810412
Victor Prado <prado.victor1005@gmail.com>
2022-09-03 02:07:53 -0300

    implementando o useState no animalForm

--- src/Components/ContactFormAnimal/index.js
+++ src/Components/ContactFormAnimal/index.js
@@ -1,56 +1,153 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 import {
   Card, FormArea, ButtonArea,
 } from './style';
+import useErrors from '../../hooks/useErros';
 
 import FormGroup from '../FormGroup/index';
+
 import Input from '../../input';
 import Button from '../../button';
+import Select from '../../select';
 
 import arrowLeft from '../../assets/images/left-arrow.png';
 
 export default function ContactFormAnimal({ titleLabel, buttonLabel }) {
+  const [name, setName] = useState('');
+  const [species, setSpecies] = useState('');
+  const [breed, setBreed] = useState('');
+  const [gender, setGender] = useState('');
+  const [nickName, setNickName] = useState('');
+  const [weigth, setWeight] = useState('');
+  const [lenght, setLenght] = useState('');
+  const {
+    errors, setError, removeError, getErrorMessageByFieldName,
+  } = useErrors();
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'nome', message: 'Preencher campo de nome' });
+    } else {
+      removeError('breed');
+    }
+  }
+
+  function handleBreedChange(event) {
+    setBreed(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'breed', message: 'Preencher campo de raça' });
+    } else {
+      removeError('breed');
+    }
+  }
+
+  function handleNickNameChange(event) {
+    setNickName(event.target.value);
+  }
+
+  function handleWeightChange(event) {
+    setWeight(event.target.value);
+  }
+
+  function handleLenghthange(event) {
+    setLenght(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!species) {
+      return console.log('Preencher campo de especie');
+    }
+
+    if (!gender) {
+      return console.log('Preencher campo de genero');
+    }
+
+    if (errors.length > 0) {
+      return;
+    }
+
+    console.log({
+      name, breed, species, nickName, weigth, lenght, gender,
+    });
+  }
+
   return (
     <Card>
       <Link to="/home">
         <img src={arrowLeft} alt="Seta para esquerda" title="Voltar" />
       </Link>
       <h1>{titleLabel}</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormArea>
-          <FormGroup>
-            <span>Nome:</span>
-            <Input />
+          <FormGroup error={getErrorMessageByFieldName('name')}>
+            <Input
+              error
+              value={name}
+              placeholder="Nome"
+              onChange={handleNameChange}
+            />
           </FormGroup>
           <FormGroup>
-            <span>Espécie:</span>
-            <Input />
+            <Select
+              value={species}
+              onChange={(event) => setSpecies(event.target.value)}
+            >
+              <option value="">Espécie</option>
+              <option value="Cachorro">Cachorro</option>
+              <option value="Gato">Gatpo</option>
+            </Select>
           </FormGroup>
           <FormGroup>
-            <span>Raça:</span>
-            <Input />
+            <Input
+              error={getErrorMessageByFieldName('breed')}
+              value={breed}
+              placeholder="Raça"
+              onChange={handleBreedChange}
+            />
           </FormGroup>
           <FormGroup>
-            <span>Sexo:</span>
-            <Input />
+            <Select
+              value={gender}
+              onChange={(event) => setGender(event.target.value)}
+            >
+              <option value="">Sexo</option>
+              <option value="Macho">Macho</option>
+              <option value="Femea">Fêmea</option>
+            </Select>
           </FormGroup>
           <FormGroup>
-            <span>Apelido:</span>
-            <Input />
+            <Input
+              placeholder="Apelido"
+              value={nickName}
+              onChange={handleNickNameChange}
+            />
           </FormGroup>
           <FormGroup>
-            <span>Peso:</span>
-            <Input />
+            <Input
+              placeholder="Peso"
+              value={weigth}
+              onChange={handleWeightChange}
+            />
           </FormGroup>
           <FormGroup>
-            <span>Comprimento:</span>
-            <Input />
+            <Input
+              placeholder="Comprimento"
+              value={lenght}
+              onChange={handleLenghthange}
+            />
           </FormGroup>
         </FormArea>
 
         <ButtonArea>
-          <Button>{buttonLabel}</Button>
+          <Button type="submit">{buttonLabel}</Button>
         </ButtonArea>
       </form>
     </Card>
---
+++ src/select.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  margin-top: 8px;
+  width: 100%;
+  height: 32px;
+  background: #FFFFDE;
+  border: none;
+  color: gray;
+  border-radius: 6px;
+  padding: 4px;
+  outline: none;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  transition: border-color 0.2s ease-in;
+`;
