75905028b4c1fa0a82a91c352d7f793f2daca22e
Victor Prado <prado.victor1005@gmail.com>
2022-09-07 01:34:33 -0300

    iniciando desenvolvimento dos toast da aplicacao

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import GlobalStyles from './assets/styles/global';
 
 import { AuthProvider } from './Context/AuthContext';
 
+// import ToastContainer from './Components/Toast/ToastContainer';
+
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <AuthProvider>
         <GlobalStyles />
+        {/* <ToastContainer /> */}
         <Router history={history}>
           <Routes />
         </Router>
