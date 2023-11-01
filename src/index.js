e5ac6a9a4dbea5fca13bfaee57bc609e4857a36e
Victor Prado <prado.victor1005@gmail.com>
2023-11-01 15:03:14 -0300

    fix: alterando favicon para o logo do petverso.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import App from './App';
 
 import './lib/EventManager';
 
-const url = process.env.REACT_APP_API_URL;
-
-console.log(url);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />,
